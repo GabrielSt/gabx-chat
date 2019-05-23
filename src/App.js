e285ec0d0e743122a8e67ec7bff0a84fdd5baae9
Gabriel Santos <g.dos.santos.pereira@concrete.com.br>
2019-05-23 00:00:30 -0300

    Creating idea for the chat layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The <code>code time</code> is about to begin.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Simple chat using react
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <aside id="sidebar">Users</aside>
+        <section id="main">
+          <section id="messages-list">Messages list</section>
+          <section id="new-message">New message</section>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
